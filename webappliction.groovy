545811fb094b9bbb93b4e2d13b9707f338a7d6f2
Ashok Chandra Reddy Tippaluri <96752472+AshokTippaluri@users.noreply.github.com>
2025-01-29 14:33:11 +0530

    Update webappliction.groovy

--- webappliction.groovy
+++ webappliction.groovy
@@ -1,1 +1,19 @@
+node{
+  stage('My first stage') {
+    echo 'Hello world!'
+  }
 
+  stage('My second stage') {
+    def message = 'Hello world'
+
+    echo 'Greeting: ${message}'
+    echo "Greeting: ${message}"
+  }
+
+  def stageNum = 'third'
+
+  stage("My ${stageNum} stage") {
+    echo 'Workspace:'
+    sh(script: 'ls -la')
+  }
+}
